cef9d59083c0cce25c59b6858472956f4e42af32
Antonio Bruchi <antoniobruchi@hotmail.com>
2025-03-18 01:00:03 +0000

    Feat: add method to save files

--- custom_assistant/utils.py
+++ custom_assistant/utils.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from werkzeug.utils import secure_filename
+from custom_assistant import ALLOWED_EXTENSIONS, UPLOAD_FOLDER
 
 
 def get_proprietary_hardware_status() -> bool:
@@ -17,3 +19,49 @@ def get_proprietary_hardware_status() -> bool:
         return res["chat_server"], res["embedding_server"]
     except:
         return chat_server, embedding_server
+
+
+def allowed_file(filename):
+    """Method to check if a file can be uploaded
+
+    Args:
+        filename (str): the fhe name of the file
+
+    Returns:
+        bool: true or false
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def save_file(request, user_id):
+    """Method to save the file the user is uploading
+
+    Args:
+        request (flask.request): the flask request
+        user_id (int): the user id
+
+    Returns:
+        bool, str | None: True and the filename if successfull
+        False and None if not
+    """
+    # check if the post request has the file part
+    if 'file_input' not in request.files:
+        print('No file part')
+        return False, None
+    file = request.files['file_input']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        print('No selected file')
+        return False, None
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        print(filename)
+        save_path = f"{UPLOAD_FOLDER}/{user_id}"
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        file.save(f"{save_path}/{filename}")
+        return True, filename
+    else:
+        return False, None
