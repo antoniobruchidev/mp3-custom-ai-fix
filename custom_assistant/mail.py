1f3c24cf2c15fcb14f5e8d97dd4456de2e80ce20
Antonio Bruchi <antoniobruchi@hotmail.com>
2025-03-24 03:07:06 +0000

    Maint: removed localhost link from emails

--- custom_assistant/mail.py
+++ custom_assistant/mail.py
@@ -36,7 +36,6 @@ def send_activation_email(user):
     msg.body = (
         f"Hello {user.email},\n\n"
         f"please click on the link below to activate your account.\n\n"
-        f"http://localhost:5000/verify/{user.id}\n\n"
         f"{HEROKU_DOMAIN}/verify/{user.id}\n\n"
         "Thanks."
     )
@@ -62,7 +61,6 @@ def forgot_password_email(user):
         f"Hello {user.email},\n\n"
         f"please click on the link below to reset your password.\n\n"
         f"If it wasn't you and we suggest you change your password anyway."
-        f"http://localhost:5000/change_password/{hash}\n\n"
         f"{HEROKU_DOMAIN}/change_password/{hash}\n\n"
         "Thanks."
     )
